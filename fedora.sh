f9581508ad32d8b35d02c140dca7afd389bafdd3
fagnersutel <fagnersutel@hotmail.com>
2018-04-28 18:47:20 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -6,13 +6,13 @@ sudo yum -y upgrade
 sudo yum install -y numpy opencv*
 
 #Instalamos as dependencias necessárias
-sudo dnf  -y groupinstall 'Development Tools'
-sudo dnf  -y install gcc-c++ 
-sudo dnf  -y install rpm-build 
-sudo dnf  -y install python-devel
-sudo dnf  -y install python3-devel 
-sudo dnf  -y install opencv-devel 
-sudo dnf  -y install opencv-python
+sudo dnf  groupinstall 'Development Tools' -y 
+sudo dnf  install gcc-c++  -y 
+sudo dnf  install rpm-build  -y 
+sudo dnf  install python-devel -y 
+sudo dnf  install python3-devel  -y 
+sudo dnf  install opencv-devel  -y 
+sudo dnf  install opencv-python -y 
 
 #Instalamos o PIP
 sudo dnf install  -y python-pip 
