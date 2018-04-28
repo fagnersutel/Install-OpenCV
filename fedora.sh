a8f82d73a5b9cab78e37fa7fa4810a141b8ea1bb
fagnersutel <fagnersutel@hotmail.com>
2018-04-28 17:03:51 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -6,17 +6,17 @@ sudo yum -y upgrade
 sudo yum install -y numpy opencv*
 
 #Instalamos as dependencias necessárias
-sudo dnf groupinstall 'Development Tools' -y 
-sudo dnf install gcc-c++ -y 
-sudo dnf install rpm-build -y 
-sudo dnf install python-devel -y 
-sudo dnf install python3-devel -y 
-sudo dnf install opencv-devel -y 
-sudo dnf install opencv-python -y 
+sudo dnf  -y groupinstall 'Development Tools'
+sudo dnf  -y install gcc-c++ 
+sudo dnf  -y install rpm-build 
+sudo dnf  -y install python-devel
+sudo dnf  -y install python3-devel 
+sudo dnf  -y install opencv-devel 
+sudo dnf  -y install opencv-python
 #Instalamos o PIP
-sudo dnf install python-pip -y 
-sudo dnf install python3-pip -y 
+sudo dnf install  -y python-pip 
+sudo dnf install  -y python3-pip
 #instalamos jupyter para Python 2 e 3
-sudo pip3 install jupyter -y 
-sudo python2 -m pip install ipykernel -y 
-sudo python2 -m ipykernel install -y 
+sudo pip3 install  -y jupyter 
+sudo python2 -m  -y pip install ipykernel
+sudo python2 -m  -y ipykernel install
